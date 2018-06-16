b32391943674cff8880146b1d3def3638cbe30ae
Levy Medina II <levymedina3@gmail.com>
2018-06-17 05:56:33 +0800

    added template for root js file

--- commands/create/create.js
+++ commands/create/create.js
@@ -34,27 +34,32 @@ let create = async () => {
   let projVersion = answers.version
   let projDescription = answers.description
 
-  // Check if folder with project name doesn't exist
-  if (!fs.existsSync(projName)) {
-    try {
-      // Create project directory
-      fs.mkdirSync(`${process.cwd()}/${projName}`)
+  try {
+    // Create project directory
+    fs.mkdirSync(`${process.cwd()}/${projName}`)
+    
+    // Create folder structure
+    fs.mkdirSync(`${process.cwd()}/${projName}/user-interface`)
+    fs.mkdirSync(`${process.cwd()}/${projName}/server`)
 
-      PACKAGE_JSON_TEMPLATE.name =  `${projName}`
-      PACKAGE_JSON_TEMPLATE.version = `${projVersion}`,
-      PACKAGE_JSON_TEMPLATE.description = `${projDescription}`,
-      
-      // Write package file
-      fs.writeFile(`${process.cwd()}/${projName}/package.json`,
-        JSON.stringify(PACKAGE_JSON_TEMPLATE, null, 4), (err) => {
-          if (err) return console.log(err)
-          console.log(`Project ${projName} has been created`)
-      })
-    } catch (err)  {
-      console.log(err)
-    }
-  } else {
-    console.log(`Project ${projName} already exists`)
+    // Create package.json file
+    PACKAGE_JSON_TEMPLATE.name =  `${projName}`
+    PACKAGE_JSON_TEMPLATE.version = `${projVersion}`,
+    PACKAGE_JSON_TEMPLATE.description = `${projDescription}`,
+
+    fs.writeFile(`${process.cwd()}/${projName}/package.json`,
+      JSON.stringify(PACKAGE_JSON_TEMPLATE, null, 4), (err) => {
+        if (err) throw new Error(err)
+    })
+
+    // Create files
+    fs
+      .createReadStream(__dirname + '/../../templates/app.js')
+      .pipe(fs.createWriteStream(`${process.cwd()}/${projName}/app.js`))
+
+    console.log(`Project ${projName} has been created`)
+  } catch (err)  {
+    console.log(err)
   }
 }
 
